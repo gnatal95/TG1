693e975400709b5fd6e976bf4e55487545e310ef
gnatal95 <guilherme.natal47@gmail.com>
2017-04-11 21:50:48 -0300

    use TimeNow variable

--- Scripts/AutoDestroy.cs
+++ Scripts/AutoDestroy.cs
@@ -1,21 +1,22 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class AutoDestroy : MonoBehaviour {
-    private float crono;
-    public float timenow;
-    public float ExTime;
+    	private float crono;
+    	public float timeNow;
+    	public float ExTime;
 	// Use this for initialization
+
 	void Start () {
-        crono = Time.fixedTime;
-        ExTime = 2;
+        	crono = Time.fixedTime;
+        	ExTime = 2;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        timenow = Time.fixedTime;
-        if (Time.fixedTime > crono + ExTime) {
-            Destroy(gameObject);
-        }
+	        timeNow = Time.fixedTime;
+		if (timeNow > crono + ExTime) {
+        		    Destroy(gameObject);
+	        }
 	}
 }
