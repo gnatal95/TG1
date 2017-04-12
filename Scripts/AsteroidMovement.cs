7fdb16075ccab598a8b9d91f5af1da44361c6c8c
gnatal95 <guilherme.natal47@gmail.com>
2017-04-11 21:49:26 -0300

    limits are no more hard coded

--- Scripts/AsteroidMovement.cs
+++ Scripts/AsteroidMovement.cs
@@ -1,20 +1,20 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class AsteroidMovement : MonoBehaviour {
 
+	public int verticalLimit = 500;
+	public int horizontalLimit = 100;
+
     public Vector3 Direction;
     void Update () 
     {
-		//evita que o asteroide saia dos limites do jogo
-        if (transform.position.x > 500)
+		if (transform.position.x > verticalLimit)
             Direction.x = -Direction.x;
         var x = Direction.x * Time.deltaTime;
-		// ??d
-        if (transform.position.y > 100 || transform.position.y < -100)
+		if (transform.position.y > horizontalLimit || transform.position.y < -horizontalLimit)
             Direction.y = -Direction.y;
         
-
         var y = Direction.y * Time.deltaTime;
 
         var z = Direction.z;
